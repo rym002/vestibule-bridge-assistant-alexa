42dc3d10ebb03f4f5648b89fc7e2a5654d85bf83
rym002 <raymond.munian@gmail.com>
2019-08-27 08:05:32 -0400

    export Alexa Emitters

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { init } from './iot';
 import { registerAssistant } from './endpoint';
 
 export { DirectiveHandlers, SupportedDirectives } from './directive'
-export { CapabilityEmitter, InfoEmitter, StateEmitter } from './endpoint'
+export { CapabilityEmitter, InfoEmitter, StateEmitter, AlexaEndpointEmitter, AlexaDirectiveEmitter, AlexaStateEmitter } from './endpoint'
 
 listenInit('assistant', async () => {
     registerModule({
