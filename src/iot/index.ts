f0ff817598c95b89776835a6f96c32ac8fff5c41
rym002 <raymond.munian@gmail.com>
2020-12-04 17:20:14 -0500

    Correct CA path

--- src/iot/index.ts
+++ src/iot/index.ts
@@ -5,20 +5,27 @@ export function alexaConfig() {
     return {
         certPath: process.env['CERT_PATH'],
         clientId: process.env['CLIENT_ID'],
-        host: process.env['VESTIBULE_HOST']
+        host: process.env['VESTIBULE_HOST'],
+        mqttLogLevel: process.env['AWS_MQTT_LOG_LEVEL']
     }
 
 }
 
 export async function createConnection(): Promise<mqtt.MqttClientConnection> {
-    const bootstrap = new io.ClientBootstrap()
-
-    const client = new mqtt.MqttClient(bootstrap)
     const appConfig = alexaConfig()
+
+    if (appConfig.mqttLogLevel) {
+        const level: io.LogLevel = parseInt(io.LogLevel[appConfig.mqttLogLevel.toUpperCase()]);
+        io.enable_logging(level);
+    }
+
+    const bootstrap = new io.ClientBootstrap()
+    const client = new mqtt.MqttClient(bootstrap)
     const config = iot.AwsIotMqttConnectionConfigBuilder
         .new_mtls_builder_from_path(appConfig.certPath + '/vestibule.cert.pem', appConfig.certPath + '/vestibule.private.key')
-        .with_certificate_authority(appConfig.certPath + '/AmazonRootCA1.pem')
+        .with_certificate_authority_from_path(undefined, appConfig.certPath + '/AmazonRootCA1.pem')
         .with_client_id(appConfig.clientId)
+        .with_clean_session(false)
         .with_endpoint(appConfig.host)
         .build()
 
